a7bca2279dd6f1d60f5e71cd29e59e5617f30aaf
aryannjainn <aryanjainmahalwa123@gmail.com>
2024-04-13 16:00:59 +0530

    final commit

---
+++ course-catalog/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{jsx,ts,tsx,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bgDark: "#22223b",
+        bgDark2: "#4a4e69",
+      },
+    },
+  },
+  plugins: [],
+}
+
